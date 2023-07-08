17214f7ab856a6961a2e88d6ddcfd770765c1dbc
Ruben Pla Ferrero <rubenpla.develop@gmail.com>
2023-07-08 17:12:41 +0200

    feat: Scaffold component -> SnackBar
    - Section 15/73

--- app/src/main/java/com/rubenpla/jetpackcomposecatalog/Scaffold.kt
+++ app/src/main/java/com/rubenpla/jetpackcomposecatalog/Scaffold.kt
@@ -1,7 +1,6 @@
 package com.rubenpla.jetpackcomposecatalog
 
 import android.annotation.SuppressLint
-import android.widget.Toast
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Build
@@ -10,27 +9,40 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
+import kotlinx.coroutines.launch
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ScaffoldExample() {
-    Scaffold(topBar = { myTopAppBar() }) {
+    //TODO this is necessary in order to show a Snackbar()
+    val scaffoldState = remember { SnackbarHostState() }
+    val coroutineScope = rememberCoroutineScope()
 
+    Scaffold(topBar = {
+        MyTopAppBar {
+            coroutineScope.launch {
+                scaffoldState.showSnackbar("$it clicked!")
+            }
+        }
+    },
+        snackbarHost = { SnackbarHost(hostState = scaffoldState) }) {
     }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun myTopAppBar() {
-
-    val context = LocalContext.current
+fun MyTopAppBar(onClickAction: (String) -> Unit) {
+    //val context = LocalContext.current
     TopAppBar(title = { Text(text = "Toolbar Title", color = Color.White) },
         colors = TopAppBarDefaults.mediumTopAppBarColors(
             containerColor = Color.DarkGray,
@@ -38,7 +50,7 @@ fun myTopAppBar() {
         ),
         navigationIcon = {
             IconButton(onClick = {
-                Toast.makeText(context, "Back button clicked!", Toast.LENGTH_LONG).show()
+                onClickAction("Back")
             }) {
                 Icon(
                     imageVector = Icons.Filled.ArrowBack,
@@ -47,26 +59,25 @@ fun myTopAppBar() {
                 )
             }
         },
-    actions = {
-        IconButton(onClick = {
-            Toast.makeText(context, "Shopping Cart clicked!", Toast.LENGTH_LONG).show()
-        }) {
-            Icon(
-                imageVector = Icons.Filled.ShoppingCart,
-                contentDescription = "Shopping cart icon",
-                tint = Color.White
-            )
-        }
+        actions = {
+            IconButton(onClick = {
+                onClickAction("Shopping cart")
+            }) {
+                Icon(
+                    imageVector = Icons.Filled.ShoppingCart,
+                    contentDescription = "Shopping cart icon",
+                    tint = Color.White
+                )
+            }
 
-        IconButton(onClick = {
-            Toast.makeText(context, "Build action clicked!", Toast.LENGTH_LONG).show()
-
-        }) {
-            Icon(
-                imageVector = Icons.Filled.Build,
-                contentDescription = "Build Action Icon",
-                tint = Color.White
-            )
-        }
-    })
+            IconButton(onClick = {
+                onClickAction("Build Action")
+            }) {
+                Icon(
+                    imageVector = Icons.Filled.Build,
+                    contentDescription = "Build Action Icon",
+                    tint = Color.White
+                )
+            }
+        })
 }
